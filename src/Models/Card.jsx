dd34c09a818685ec6b40d84ed589d3f77de28edb
Rahul Parihar <rahulpariharmalii@gmail.com>
2025-06-13 14:17:54 +0530

    No changes on card

--- src/Models/Card.jsx
+++ src/Models/Card.jsx
@@ -14,14 +14,10 @@ const Card = ({
     <div
       className={`flex flex-col ${
         reverse ? 'md:flex-row-reverse' : 'md:flex-row'
-      } w-full max-w-8xl shadow-lg bg-white`}
-      style={{
-        minHeight: '70vh',   // Ensures good height at 100%
-        maxHeight: 'none',   // Allows content to grow with zoom
-      }}
+      } w-full max-w-8xl md:h-[80vh] overflow-hidden shadow-lg bg-white`}
     >
       {/* Left Image */}
-      <div className="w-full md:w-1/2 h-64 md:h-auto">
+      <div className="w-full md:w-1/2 h-64 md:h-full">
         <img
           src={imageUrl}
           alt="Card Visual"
@@ -53,7 +49,7 @@ const Card = ({
         {onButtonClick && (
           <button
             onClick={onButtonClick}
-            className="mt-6 md:mt-8 w-max px-5 py-2 md:px-6 md:py-3 text-white font-semibold transition-colors duration-300"
+            className="mt-6 md:mt-8 w-max px-5 py-2 md:px-6 md:py-3 text-white font-semibold"
             style={{ backgroundColor: highlightColor }}
             onMouseOver={(e) =>
               (e.currentTarget.style.backgroundColor = '#e5430a')
