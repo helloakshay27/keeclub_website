22c4b515652ee392fbf92d92c4dd67cdb6566120
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-06-21 12:02:37 +0530

    fix: ensure BASE_URL is correctly assigned based on current hostname

--- src/Confi/baseurl.js
+++ src/Confi/baseurl.js
@@ -3,7 +3,7 @@ let BASE_URL = ""
 const hostname = window.location.hostname;
 console.log("Current hostname:", hostname);
 
-if (hostname === "https://uat-keeclub.lockated.com/") {
+if (hostname === "uat-keeclub.lockated.com") {
     BASE_URL = "https://uat-loyalty.lockated.com/"
 } else {
     BASE_URL = "https://piramal-loyalty-dev.lockated.com/";
