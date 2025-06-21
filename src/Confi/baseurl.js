c6d4c3be90ef2d72af6ee1c58390464e82918d6c
fardeen-mohd-rafiq <fardeen2903@gmail.com>
2025-06-21 11:51:49 +0530

    fix: correct BASE_URL assignment logic for environment handling

--- src/Confi/baseurl.js
+++ src/Confi/baseurl.js
@@ -5,7 +5,7 @@ const hostname = window.location.hostname;
 if (hostname === "https://uat-keeclub.lockated.com/") {
     BASE_URL = "https://uat-loyalty.lockated.com/"
 } else {
-    BASE_URL = "https://piramal-loyalty-dev.lockated.com";
+    BASE_URL = "https://piramal-loyalty-dev.lockated.com/";
 }
 
 export default BASE_URL;
